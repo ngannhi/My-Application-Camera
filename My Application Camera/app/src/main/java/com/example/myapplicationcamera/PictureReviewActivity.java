b07e04c3f0628319c2379ace347996f05efd5a13
haopham96 <psinhhao96@gmail.com>
2019-05-14 14:16:44 +0700

    fix width, height pircture [new] review picture

---
+++ My Application Camera/app/src/main/java/com/example/myapplicationcamera/PictureReviewActivity.java
@@ -0,0 +1,13 @@
+package com.example.myapplicationcamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PictureReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_review);
+    }
+}
